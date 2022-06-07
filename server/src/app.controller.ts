fac2b0ee225aa75cd7cda18edd669f0d6fe2da38
Kafil <kafil.nasdami@gmail.com>
2022-06-07 03:52:20 +0100

    add a dummy endpoint

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/price')
+  getPriceWithDuration(
+    @Query('duration') duration: number,
+    @Query('startTime') startTime: string,
+    @Query('distance') distance: number,
+  ): number {
+    //TODO: validate inputs
+    return this.appService.getPrice(duration, startTime, distance);
   }
 }
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getPrice(duration: number, startTime: string, distance: number): number {
+    return duration * distance * 0.1;
   }
 }
